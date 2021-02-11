14b1185a405e5b788cf15f74c5bbd819263a90c3
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2021-02-10 19:15:15 -0800

    Add files via upload

--- src/routers/crmRouter.js
+++ src/routers/crmRouter.js
@@ -1,5 +1,5 @@
 import { addNewContact, getContact, getContactwithID, updateContact, deleteContact } from '../controllers/crmController'
-
+import { login, register, loginRequired } from './controllers/userControllers'
 // the methods of CRUD 
 // this are the end points 
 const routers = (app) => {
@@ -13,18 +13,31 @@ const routers = (app) => {
             // to past to the next function 
         next();
 
-    }, getContact)
+    }, loginRequired, getContact)
 
     //to past the contacts in the router 
-    .post(addNewContact);
+    .post(loginRequired, addNewContact);
     //to past hast to be same or it will not work contactID
     app.route('/contact/:contactID')
-        //get the contact
-        .get(getContactwithID)
-        //to put the contact in the table
-        .put(updateContact)
-        //delate the contact
-        .delate(deleteContact)
+        //get the contact and the login
+        .get(loginRequired, getContactwithID)
+        //to put the contact in the table and the login
+        .put(loginRequired, updateContact)
+        //delate the contact and the login
+        .delate(loginRequired, deleteContact)
 }
+
+//create the new router 
+//regristation route
+
+app.route('/auth/register')
+    .post(register);
+
+
+app.route('/login')
+    .post(login);
+
+
+
 
 export default routers;
